52dd47494b03d4632b149ba5c46c1a4873c71035
jorgejimenezQ <jorgejimenez@mail.weber.edu>
2023-02-18 21:12:45 -0700

    FEAT: Add calls to the onSwipeLeft, onSwipeRight, and onSwipe callbacks.

--- examples/index.js
+++ examples/index.js
@@ -3,4 +3,20 @@ import CardFlick from '../lib/card-flick.js'
 const cardContainer = document.querySelector('#card-container')
 const cards = document.querySelectorAll('.card')
 
-const cardFlick = new CardFlick(cardContainer, cards)
+const onSwipeLeft = (card) => {
+    console.log('swiped left')
+}
+
+const onSwipeRight = (card) => {
+    console.log('swiped right')
+}
+
+const onSwipe = (card) => {
+    console.log('swiped')
+}
+
+const cardFlick = new CardFlick(cardContainer, cards, {
+    onSwipeLeft,
+    onSwipeRight,
+    onSwipe,
+})
